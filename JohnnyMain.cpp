4f9b48adc0a4d405c4c62b6e56c718bb42115df9
JohnVonDrashek <32941004+JohnVonDrashek@users.noreply.github.com>
2018-01-31 11:15:31 -0600

    Updated JohnnyMain and test_resume.txt
    JohnnyMain now converts the words of the resume into tokens.
    test_resume.txt now includes a "RED" header

--- JohnnyMain.cpp
+++ JohnnyMain.cpp
@@ -1,26 +1,61 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <sstream>
 
 
-
+char specials[] = {':', '/'};
 
 using namespace std;
 
+bool isSpecialCharacter(char c)
+{
+    for(char s : specials)
+    {
+        if(c == s)
+            return true;
+    }
+    return false;
+}
+
+
 void helpEmployer()
 {
-
-    cout << "Please give resume path: ";
-    string path;
+    string path == "";
+    cout << "What is the path of the resume: ";
     getline(cin, path);
 
     ifstream resume(path);
 
-    string line;
-    while(resume >> line)
-    {
-        cout << line << endl;
-    }
+    string word;
 
+    stringstream tokens;
+
+    while(resume >> word)
+    {
+        int countSinceLastToken = 0;
+
+        int i = 0;
+        for(; i < word.length(); i++)
+        {
+            if(isSpecialCharacter(word[i]))
+            {
+                if(i > 0)
+                {
+                    tokens << word.substr(0, i) << endl;
+                }
+                countSinceLastToken = i;
+                tokens << word[i] << endl;
+                word = word.substr(i+1, word.length());
+                i = 0;
+            }
+        }
+        if(i > 0)
+        {
+            tokens << word.substr(0, i) << endl;
+        }
+
+    }
 
 
 }
